bde5ca1aa35966cbe1b076cb414bf27f57226309
Anderson Pires <anderson.pires.dc@gmail.com>
2022-07-17 09:48:50 -0300

    Commit efetuado do meu notebook

--- apaAgenda/app/src/main/java/com/example/apaagenda/MainActivity.java
+++ apaAgenda/app/src/main/java/com/example/apaagenda/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.apaagenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_Nome, et_Telefone;
+    Button btnGravar, btnConsultar, btnFechar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_Nome = findViewById(R.id.edtNomePrincipal);
+        et_Telefone = findViewById(R.id.edtTelefonePrincipal);
+
+        btnGravar = findViewById(R.id.btnGravar);
+        btnConsultar = findViewById(R.id.btnConsultar);
+        btnFechar = findViewById(R.id.btnFechar);
     }
 }
